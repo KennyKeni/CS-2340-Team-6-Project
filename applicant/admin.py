d9da635166c1aec8d37fbd5af61142c4b4bf2abc
sayandahiyagt <sdahiya31@gatech.edu>
2025-09-25 21:34:12 -0400

    User Story 4 (#5)
    * user story 10: Creating Job postings
    
    * Fixed superusererrors
    
    * Just same stuff
    
    * Completed User Story 4,(admin ref style)
    
    ---------
    
    Co-authored-by: Sayan Dahiya <sayan.dahiya@gmail.com>

--- applicant/admin.py
+++ applicant/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Application
 
-# Register your models here.
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ("job", "applicant", "status", "updated_at")
+    list_filter = ("status", "updated_at")
+    search_fields = ("job__title", "applicant__username")
