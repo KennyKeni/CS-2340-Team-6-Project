c78c650c5e6a3d535f2105fa9b84d1a9deff5b43
jxczz <banglanta111@gmail.com>
2025-10-22 02:50:11 -0400

    added additional imports (Notification, Message, SavedSearch) for the admin panel in case it's needed

--- recruiter/admin.py
+++ recruiter/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recruiter, CandidateEmail
+from .models import Recruiter, CandidateEmail, Notification, Message, SavedSearch
 
 
 @admin.register(Recruiter)
