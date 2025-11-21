ed5ea4f415617d1547595c533bea782ecc863b42
4151x <banglanta111@gmail.com>
2025-11-20 23:34:44 -0500

    adds a new URL that allows user to view job details. this allows the recruiter to see all those that applied and their ranking based on job requirements + user skills

--- recruiter/urls.py
+++ recruiter/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("jobs/", views.my_job_postings, name="jobs"),
     path("jobs/create/", views.job_create, name="job_create"),
+    path("jobs/<int:pk>/", views.job_detail, name="job_detail"),
     path("jobs/<int:pk>/edit/", views.job_update, name="job_update"),
     path("jobs/<int:pk>/delete/", views.job_delete, name="job_delete"),
     path("jobs/<int:job_id>/applications/", views.job_applications_pipeline, name="job_applications"),
